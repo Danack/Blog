050817eb20e8f1470cef5ccaeb8f6fdae65905e5
Danack <Danack@basereality.com>
2020-11-30 18:13:39 +0000

    Don't need the debug box in production.

--- runProd.sh
+++ runProd.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
 docker-compose build --no-cache
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
-
-
-
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d blog_php_fpm nginx
