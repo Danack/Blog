14f5b0f8200affd0df8d964e5f0fcdf6c1215af7
Danack <Danack@basereality.com>
2022-12-24 21:39:23 +0000

    Don't ignore composer.

--- runProd.sh
+++ runProd.sh
@@ -2,6 +2,6 @@
 
 docker-compose build --no-cache
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --build --remove-orphans installer
-# docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d blog_php_fpm nginx
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d blog_php_fpm nginx
 
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml up blog_php_fpm nginx
+# docker-compose -f docker-compose.yml -f docker-compose.prod.yml up blog_php_fpm nginx
