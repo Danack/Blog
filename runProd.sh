8761a8622873ff41c67e805ed4d50d149e0dfd96
Danack <Danack@basereality.com>
2023-07-02 15:31:16 +0100

    Build a bit faster.

--- runProd.sh
+++ runProd.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker-compose build --no-cache
+docker-compose build --no-cache blog_php_fpm nginx
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --build --remove-orphans installer
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d blog_php_fpm nginx
 
